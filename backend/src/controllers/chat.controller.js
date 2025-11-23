dd1f57cc3af202d930c321bceff255fb4255bb2e
Raghvendra Dwivedi <dwivediji425@gmail.com>
2025-11-23 17:04:36 +0530

    feat: Add Clerk authentication middleware and implement initial chat API with stream token generation.

---
+++ backend/src/controllers/chat.controller.js
@@ -0,0 +1,16 @@
+import { chatClient } from "../lib/stream"
+
+
+export const getStreamToken = async (req, res) => {
+    try {
+        //use clerk id for stream because we are saving clerk id in stream db
+        const token = await chatClient.createToken(
+            req.user.clerkId,
+        );
+        return res.status(200).json({ token, userId: req.user.clerkId, username: req.user.username, userImage: req.user.image });
+    } catch (error) {
+        console.error("Error in getStreamToken controller ", error);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+
+}   
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import path from "path";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./lib/inngest.js";
 import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
 
 const __dirname = path.resolve();
 
@@ -12,15 +13,24 @@ const app = express();
 //middlewares
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware()); //this adds auth field to req object
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 //connecting to mongodb
 import { connectDB } from "./lib/db.js";
+import { protectRoute } from "./middlewares/protectRoute.js";
+import chatRoute from "./routes/chat.route.js";
 
 app.get("/api/health", (req, res) => {
   res.send("api is running");
 });
+
+app.use("/api/auth", protectRoute, (req, res) => {
+  res.json({ message: "auth is running", user: req.user });
+});
+
+app.use("/api/chat", protectRoute, chatRoute);
 
 //make our app ready for production
 if (ENV.NODE_ENV === "production") {
